28a197d0e84d93427d908178b7e65d207fc3eee1
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-05-01 02:45:33 +0300

    print

--- print_array.c
+++ print_array.c
@@ -9,15 +9,15 @@
  */
 void print_array(const int *array, size_t size)
 {
-	size_t i;
+	size_t x;
 
-	i = 0;
-	while (array && i < size)
+	x = 0;
+	while (array && x < size)
 	{
 		if (i > 0)
 			printf(", ");
-		printf("%d", array[i]);
-		++i;
+		printf("%d", array[x]);
+		++x;
 	}
 	printf("\n");
 }
